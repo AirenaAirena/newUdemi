4caed2e4c1845b5ce5b0df996c276f7576656465
AireneAirene <airenekara@gmail.com>
2025-04-14 23:45:05 +0200

    Selecting quoe from the arraz of quotes when the button when the button is clicked

---
+++ script.js
@@ -0,0 +1,18 @@
+const quotes = [
+  'The grootste glorie in het leven ligt niet in nooit vallen, maar in elke keer dat we opstaan. – Nelson Mandela',
+  'Wie naar buiten kijkt, droomt; wie naar binnen kijkt, ontwaakt. – Carl Jung',
+  'Het leven is als fietsen – om in balans te blijven, moet je in beweging blijven. – Albert Einstein',
+];
+
+const quoteElement = document.getElementById('quote');
+const generateBtn = document.getElementById('generate-btn');
+
+function generateRandomQuote() {
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  const randomQuote = quotes[randomIndex];
+  quoteElement.textContent = randomQuote;
+}
+
+generateBtn.addEventListener('click', generateRandomQuote);
+
+generateRandomQuote();
