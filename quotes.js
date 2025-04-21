ab00d4e55c5d4aba71952d34a418ce50a4da7440
AireneAirene <airenekara@gmail.com>
2025-04-21 23:05:28 +0200

    Show favorite quotess below current quote

--- quotes.js
+++ quotes.js
@@ -3,45 +3,55 @@ const quotes = [
     quote:
       'De grootste glorie in het leven ligt niet in nooit vallen, maar in elke keer dat we opstaan.',
     autor: 'Nelson Mandela',
+    isFavorite: false,
   },
   {
     quote: 'Wie naar buiten kijkt, droomt; wie naar binnen kijkt, ontwaakt.',
     autor: 'Carl Jung',
+    isFavorite: false,
   },
   {
     quote:
       'Het leven is als fietsen – om in balans te blijven, moet je in beweging blijven.',
     autor: 'Albert Einstein',
+    isFavorite: false,
   },
   {
     quote: 'Wees de verandering die je in de wereld wilt zien.',
     autor: 'Mahatma Gandhi',
+    isFavorite: false,
   },
   {
     quote: 'In het midden van moeilijkheden liggen kansen.',
     autor: 'Albert Einstein',
+    isFavorite: false,
   },
   {
     quote: 'Het enige wat we hoeven te vrezen, is de angst zelf.',
     autor: 'Franklin D. Roosevelt',
+    isFavorite: false,
   },
   {
     quote:
       'Succes is niet het sleutel tot geluk. Geluk is de sleutel tot succes.',
     autor: 'Albert Schweitzer',
+    isFavorite: false,
   },
   {
     quote: 'Twijfel doodt meer dromen dan falen ooit zal doen.',
     autor: 'Suzy Kassem',
+    isFavorite: false,
   },
   {
     quote: 'Wat je vandaag doet, bepaalt je morgen.',
     autor: 'Ralph Marston',
+    isFavorite: false,
   },
   {
     quote:
       'Geluk is niet iets kant-en-klaars. Het komt voort uit je eigen daden.',
     autor: 'Dalai Lama',
+    isFavorite: false,
   },
 ];
 
--- script.js
+++ script.js
@@ -3,17 +3,47 @@ import quotes from './quotes.js';
 const quoteElement = document.getElementById('quote');
 const quoteAutorElement = document.getElementById('quote-autor');
 const generateBtn = document.getElementById('generate-btn');
+const toggleFavoriteBtn = document.getElementById('toggle-favorite-btn');
+const favoritesContainer = document.getElementById('favorites-container');
+
+let currentQuoteIndex;
 
 function generateRandomQuote() {
-  const randomIndex = Math.floor(Math.random() * quotes.length);
-  const randomQuote = quotes[randomIndex];
+  currentQuoteIndex = Math.floor(Math.random() * quotes.length);
+  const randomQuote = quotes[currentQuoteIndex];
   const { quote, autor } = randomQuote;
-  // const quote = randomQuote.quote;
-  // const quoteAutor = randomQuote.autor;
+
   quoteElement.textContent = quote;
   quoteAutorElement.textContent = autor;
+  toggleFavoriteBtn.textContent = randomQuote.isFavorite
+    ? 'Remove from favorites'
+    : 'Add to favorites';
+
+  toggleFavoriteBtn.style.display = 'inline-block';
+}
+
+function toggleFavorite() {
+  const currentQuote = quotes[currentQuoteIndex];
+  currentQuote.isFavorite = !currentQuote.isFavorite;
+  toggleFavoriteBtn.textContent = currentQuote.isFavorite
+    ? 'Remove from favorites'
+    : 'Add to favorites';
+  if (currentQuote.isFavorite) {
+    const favoriteCard = document.createElement('div');
+    favoriteCard.classList.add('favorite-card');
+    favoriteCard.innerHTML = `<p>${currentQuote.quote}</p><p class = "autor">${currentQuote.autor}<p>`;
+    favoritesContainer.appendChild(favoriteCard);
+  } else {
+    const favoriteCards = document.querySelectorAll('.favorite-card');
+    favoriteCards.forEach((card) => {
+      if (card.textContent.includes(currentQuote.quote)) {
+        card.remove();
+      }
+    });
+  }
 }
 
 generateBtn.addEventListener('click', generateRandomQuote);
+toggleFavoriteBtn.addEventListener('click', toggleFavorite);
 
 //generateRandomQuote();
